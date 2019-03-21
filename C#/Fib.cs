26b075d63ff300cf18034f7a1638b3c23900c6d1
A1rPun <thombakker@gmail.com>
2019-03-22 00:26:48 +0100

    :copyright: Add C#

---
+++ C#/Fib.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Math;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = 29;
+            Console.WriteLine(FibLinear(input));
+            Console.WriteLine(FibFormula(input));
+        }
+
+        static int Fib(int n)
+        {
+            return n < 2 ? n : Fib(n - 1) + Fib(n - 2);
+        }
+
+        static int FibLinear(int n)
+        {
+            if (n < 2) return n;
+            var previousFib = 0;
+            var fib = 1;
+            while (--n) (previousFib, fib) = (fib, previousFib + fib);
+            return fib;
+        }
+
+        static int FibFormula(int n)
+        {
+            return Math.Round(((5 ** .5 + 1) / 2) ** n / 5 ** .5);
+        }
+    }
+}
---
+++ C#/Helloworld.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+        }
+    }
+}
