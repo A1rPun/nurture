59fadfb4099f14b8123ea244bc741097ca8768d4
A1rPun <thombakker@gmail.com>
2019-04-08 12:12:26 +0200

    :shell: Fibonacci linear in go

--- go/fib.go
+++ go/fib.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+  "os"
   "fmt"
+  "strconv"
 )
 
 func main() {
-  const n = 29
-  fibonacci := fib(n)
-  fmt.Printf("%d = %d\n", n, fibonacci)
+  input, err := strconv.Atoi(os.Args[1])
+  if err != nil {
+    input = 29
+  }
+  fibonacci := fibLinear(input)
+  fmt.Printf("%d = %d\n", input, fibonacci)
 }
 
 func fib(n int) int {
@@ -15,4 +20,13 @@ func fib(n int) int {
     return n
   }
   return fib(n - 1) + fib(n - 2)
+}
+
+func fibLinear(n int) int {
+  previousFib := 0
+  fib := 1
+  for ;n > 1; n-- {
+    fib, previousFib = fib + previousFib, fib
+  }
+  return fib
 }
