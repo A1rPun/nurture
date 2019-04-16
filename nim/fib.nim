03453edfa33406bad50988c04a79ae9bf6aa3aa6
Thom Bakker <t.bakker@ib.nl>
2019-04-16 18:37:10 +0200

    :shell: Fibonacci in nim

---
+++ nim/fib.nim
@@ -0,0 +1,30 @@
+import os
+import strutils
+
+proc fib(n: int): int64 =
+  if n < 2:
+    result = n
+  else:
+    result = fib(n - 1) + fib(n - 2)
+
+proc fibTail(n: int, prevFib: int64, fib: int64): int64 =
+  if n == 0:
+    result = prevFib
+  else:
+    result = fibTail(n - 1, fib, prevFib + fib)
+proc fibTail(n: int): int64 =
+  result = fibTail(n, 0, 1)
+
+proc fibLinear(n: int): int =
+  var
+    prevFib = 0
+    fib = 1
+    i = n
+  while i > 1:
+    swap prevFib, fib
+    fib += prevFib
+    dec i
+  result = fib
+
+var input = if paramCount() > 0: parseInt(paramStr(1)) else: 29
+echo fibTail(input)
