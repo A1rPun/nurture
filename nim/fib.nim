48894bcdec4df321763a1a11ea9ec09664859767
A1rPun <thombakker@gmail.com>
2019-04-27 18:01:41 +0200

    :wrench: Use int64 in nim

--- nim/fib.nim
+++ nim/fib.nim
@@ -1,21 +1,21 @@
 import os
 import strutils
 
-proc fib(n: int): int64 =
+proc fib(n: int64): int64 =
   if n < 2:
     result = n
   else:
     result = fib(n - 1) + fib(n - 2)
 
-proc fibTail(n: int, prevFib: int64, fib: int64): int64 =
+proc fibTail(n: int64, prevFib: int64, fib: int64): int64 =
   if n == 0:
     result = prevFib
   else:
     result = fibTail(n - 1, fib, prevFib + fib)
-proc fibTail(n: int): int64 =
+proc fibTail(n: int64): int64 =
   result = fibTail(n, 0, 1)
 
-proc fibLinear(n: int): int =
+proc fibLinear(n: int64): int64 =
   var
     prevFib = 0
     fib = 1
@@ -27,4 +27,4 @@ proc fibLinear(n: int): int =
   result = fib
 
 var input = if paramCount() > 0: parseInt(paramStr(1)) else: 29
-echo fibTail(input)
+echo fibLinear(input)
