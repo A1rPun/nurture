73e3a1e5151ca2b32dda9452c20687cb9ebb39f4
A1rPun <thombakker@gmail.com>
2019-09-10 23:47:56 +0200

    :snail: Actually fix C# fibonacci

--- csharp/Fib.cs
+++ csharp/Fib.cs
@@ -2,32 +2,36 @@
 
 namespace Fibonacci
 {
-    class Program
+  public class Program
+  {
+    public static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            var input = 29;
-            Console.WriteLine(FibLinear(input));
-        }
-
-        static int Fib(int n)
-        {
-            return n < 2 ? n : Fib(n - 1) + Fib(n - 2);
-        }
-
-        static int FibLinear(int n)
-        {
-            if (n < 2) return n;
-            var prevFib = 0;
-            var fib = 1;
-            while (--n == 0)
-                (prevFib, fib) = (fib, prevFib + fib);
-            return fib;
-        }
-
-        static int FibFormula(int n)
-        {
-            return (int)Math.Round(Math.Pow((Math.Pow(5, .5) + 1) / 2, n) / Math.Pow(5, .5));
-        }
+      int input = args.Length > 0 ? int.Parse(args[0]) : 29;
+      Console.WriteLine(FibLinear(input));
     }
+
+    static decimal Fib(int n)
+    {
+      return n < 2 ? n : Fib(n - 1) + Fib(n - 2);
+    }
+
+    static decimal FibLinear(int n)
+    {
+      if (n < 2) return n;
+      var prevFib = 0;
+      var fib = 1;
+      while (--n > 0)
+      {
+        var temp = prevFib + fib;
+        prevFib = fib;
+        fib = temp;
+      }
+      return fib;
+    }
+
+    static decimal FibFormula(int n)
+    {
+      return (decimal)Math.Round(Math.Pow((Math.Pow(5, .5) + 1) / 2, n) / Math.Pow(5, .5));
+    }
+  }
 }
