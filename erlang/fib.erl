ead30d12b986b7274e962659d7add671ead76564
A1rPun <thombakker@gmail.com>
2021-11-09 20:48:40 +0100

    :snail: erlang fast doubling

--- erlang/fib.erl
+++ erlang/fib.erl
@@ -1,8 +1,10 @@
 -module(fib).
 -import(math, [pow/2]).
+-import(lists, [nth/2]).
 -export([recursive/1]).
 -export([formula/1]).
 -export([tail_recursive/1]).
+-export([fast_doubling/1]).
 -export([formal/1]).
 
 recursive(N) when N < 2 -> N;
@@ -13,6 +15,19 @@ formula(N) -> round(pow((pow(5, 0.5) + 1) / 2, N) / pow(5, 0.5)).
 tail_recursive(N) -> tail_recursive(N, 0, 1).
 tail_recursive(0, PrevFib, _) -> PrevFib;
 tail_recursive(N, PrevFib, Fib) -> tail_recursive(N - 1, Fib, PrevFib + Fib).
+
+fast_auxiliary(0) -> [0, 1];
+fast_auxiliary(N) ->
+  fibs = fast_auxiliary(N div 2),
+  prevFib = nth(1, fibs),
+  fib = nth(2, fibs),
+  a = prevFib * (fib * 2 - prevFib),
+  b = prevFib * prevFib + fib * fib,
+  if
+    n rem 2 == 0 -> [a, b];
+    true -> [b, a + b]
+  end.
+fast_doubling(N) -> nth(1, fast_auxiliary(N)).
 
 formal(0) -> 0;
 formal(1) -> 1;
